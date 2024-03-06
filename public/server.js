ff64d61c9e4b643e9f5b738af7521cdaba6eb7ac
Alexander Scarlett <ascarlett92@yahoo.com>
2024-03-06 11:35:53 -0500

    started code for challenge 2 added const express, a path, and a heroku port

---
+++ public/server.js
@@ -0,0 +1,14 @@
+const express= require("express")
+const path=require("path")
+// process.env.PORT is used for live deployment to heroku so heroku can assign a port number to identify a server.
+const PORT = process.env.PORT || 3001
+
+const app=express()
+
+
+
+
+
+
+
+app.listen()
