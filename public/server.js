91ef3643f4ef657d9bf4e1301e35ab7a014eb23b
Alexander Scarlett <ascarlett92@yahoo.com>
2024-05-27 19:46:43 -0400

    updated server.js file

--- public/server.js
+++ public/server.js
@@ -3,34 +3,27 @@ const path=require("path")
 // process.env.PORT is used for live deployment to heroku so heroku can assign a port number to identify a server.
 const PORT = process.env.PORT || 3001
 const app=express()
-const fs =require("fs")
+const route = require("./routes/index")
 
 // added middleware browser (input firstname => "alex") -> server.js
 //extended exact data from original after parsing.
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.use("/api", route)
 
 
 
 
-//api routes
-app.get("/api/notes",(req,res)=> {
-    fs.readFile("./db/db.json")
+
+app.get("/notes",(req,res)=> {
+    res.sendFile(path.join(_dirname, "./public/notes.html"))
 })
 
-app.post("/api/notes",(req,res)=>{
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(_dirname), "./public/index.html")
+})
     
-})
-
-//html routes
-//http://localhost:3001/*
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,'./public/index/html'))
-})
-
-app.get("/notes", ()=>{
-   fs.readFile("./assets/db/db.json",${color})
-})
 
 
 app.listen(PORT, ()=>{
