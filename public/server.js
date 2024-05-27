473228f926462e1d8c7abfae45eb9cd8ec8deb28
Alexander Scarlett <ascarlett92@yahoo.com>
2024-05-27 19:35:25 -0400

    renamed to server from unmodular

--- public/server.js
+++ public/server.js
@@ -2,20 +2,20 @@ const express= require("express")
 const path=require("path")
 // process.env.PORT is used for live deployment to heroku so heroku can assign a port number to identify a server.
 const PORT = process.env.PORT || 3001
-
 const app=express()
+const fs =require("fs")
 
 // added middleware browser (input firstname => "alex") -> server.js
 //extended exact data from original after parsing.
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-const fs =require("fs")
+
 
 
 //api routes
 app.get("/api/notes",(req,res)=> {
-
+    fs.readFile("./db/db.json")
 })
 
 app.post("/api/notes",(req,res)=>{
@@ -28,10 +28,8 @@ app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,'./public/index/html'))
 })
 
-app.use("/notes", ()=>{
-    res.sendFile(err=>{
-       path.join( _dirname , "./note.html")
-    })
+app.get("/notes", ()=>{
+   fs.readFile("./assets/db/db.json",${color})
 })
 
 
