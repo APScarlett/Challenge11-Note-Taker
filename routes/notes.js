5edb1ada8e0a083873dd47c311cf0f6f022e916a
Alexander Scarlett <ascarlett92@yahoo.com>
2024-05-27 20:05:47 -0400

    updated notes.js

---
+++ routes/notes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router()
+const fs = require("fs")
+const path =require("path")
+
+
+router.get("/",(req,res)=>{
+    fs.readFile("./db/db/json", "utf8",(err, data)=>{
+        const newData =JSON.parse(data)
+        res.json(newData)
+    })
+})
+
+router.post("/", (req,res)=>{
+    fs.readFile(".db/db.json", "utf8", (err, data)=>{
+        const newData =JSON.parse(data)
+        newData.push(req.body)
+        fs.writeFile("./db/db.json", JSON.stringify(newData, null, "\t"),(err)=>{
+            res.json(newData)
+        })
+    })
+})
+
+module.exports = router
